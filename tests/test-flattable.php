300304b44671cd80806dac30a69b9da402095317
Claudio Frasca <c.frasca@krone.at>
2018-02-21 14:19:19 +0100

    basic unit test

--- tests/test-flattable.php
+++ tests/test-flattable.php
@@ -8,19 +8,25 @@
  * Testcases for the REST API demo plugin
  */
 
-class TestFlattable extends WP_UnitTestCase
+namespace KMM\Flattable;
+
+class TestFlattable extends \WP_UnitTestCase
 {
     public function setUp()
     {
         # setup a rest server
         parent::setUp();
+
+        $this->core = new Core('i18n');
     }
 
-    // dummy test -> fixme
-    public function testTrueIsTrue()
+    public function test_save_post_no_post()
     {
-        $foo = true;
-        $this->assertTrue($foo);
+        $postId = 1;
+        $postObject = '';
+        $update = true;
+        $save = $this->core->save_post($postId, $postObject, $update);
+        $this->assertNull($save);
     }
 
     public function tearDown()
